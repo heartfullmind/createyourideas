6ca3436f189916e85276272351fdbec999679399
Manuel Miklo <ceo@heartfull-mind.org>
2020-01-16 10:50:56 +0100

    pledge updatew

--- src/main/webapp/app/pledge/pledge.module.ts
+++ src/main/webapp/app/pledge/pledge.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PledgeRoutingModule } from './pledge-routing.module';
 import { PledgeComponent } from './pledge.component';
+import { HomeSharedModule } from 'app/shared/shared.module';
 
 
 @NgModule({
   declarations: [PledgeComponent],
   imports: [
+    HomeSharedModule,
     CommonModule,
     PledgeRoutingModule
   ]
